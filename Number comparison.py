40e2cff09d60b3e118a272373b94ea5e00b770ec
Ramis Mustafa <ramis.mustafa9@gmail.com>
2020-01-26 17:19:26 +0500

    Intro to conditional statements in Python (3+)

--- Number comparison.py
+++ Number comparison.py
@@ -1,7 +1,7 @@
 print("\t\t\tEnter numbers for comparison")
 
-Num1=float(input("Enter Integer 1:"))
-Num2=float(input("\nEnter Integer 2:"))
+Num1=int(input("Enter Number 1:"))
+Num2=int(input("\nEnter Number 2:"))
 
 input("\n\tPress Enter")
 
@@ -9,8 +9,8 @@ if Num1==Num2:
     print("\nNumber 1 ("+str(Num1)+") is equal to Number 2 ("+str(Num2)+")")
 else:
     if Num1>Num2:
-        print("\nInteger 1 ("+str(Num1)+") is larger than Number 2 ("+str(Num2)+")")
+        print("\nNumber 1 ("+str(Num1)+") is larger than Number 2 ("+str(Num2)+")")
     else:
-        print("\nInteger 2 ("+str(Num2)+") is larger than Integer 1 ("+str(Num1)+")")
+        print("\nNumber 2 ("+str(Num2)+") is larger than Number 1 ("+str(Num1)+")")
         
 input("\n\nPress enter to exit")
